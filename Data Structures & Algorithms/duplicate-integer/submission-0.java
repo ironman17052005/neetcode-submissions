353674846a4da07bcbf0510d54d3676286e3f621
Clark Duy Nguyen <ironman17052005@gmail.com>
2026-07-26 16:23:11 -0500

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int num = nums[i];
+
+            if(map.containsKey(num)){
+                    
+                return true;
+            }
+
+
+        map.put(num, i);
+
+        }
+            return false;
+
+    }
+}
